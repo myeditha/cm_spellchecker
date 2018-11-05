25106ced087b4d7e1eccbaa5bebff3d6b1346e88
myeditha <myeditha@andrew.cmu.edu>
2018-11-04 22:32:56 -0500

    refactored code

--- spellcheck/src/main.py
+++ spellcheck/src/main.py
@@ -4,11 +4,12 @@ from .spellcheck import *
 
 def spellcheckEntry():
 	parser = argparse.ArgumentParser(description='Code-mixed spellchecking')
-	parser.add_argument('file', metavar = 'F', type=str, help='file with code-mixed content, separated by newlines.')
+	parser.add_argument('file', metavar = 'F', type=str, help='file with newline-separated code-mixed content.')
 	parser.add_argument('-A', default = 1,type=int, help='least-interference normalization (default)')
 	parser.add_argument('-outputType', default = "firstOf", type=str, help='what your program outputs. Options: \'firstOf\', \'lattice\'')
 	parser.add_argument('-langTag', default = None, type=str, help='the non-English language in the code-mixed text. If not included, tags text for you.')
-	parser.add_argument('-tagDoc',default = None, type=str,help='text is tagged with language id')
+	parser.add_argument('-tagDoc',default = None, type=str,help='the text file containing words tagged with language id (word and language denoted)')
+	parser.add_argument('-dictDoc',default = None, type=str,help='dictionary of words in language denoted by -langTag.')
 	parser.add_argument('-repklEng', action='store_true',help='remake and repickle the English spellchecker object')
 
 	args = parser.parse_args()
@@ -16,7 +17,14 @@ def spellcheckEntry():
 	if(args.langTag != "tel"):
 		print("")
 
-	spellchecker = Spellchecker(args.repklEng, args.A, args.outputType, args.tagDoc)
+	if(not args.langTag):
+		print("Must specify non-English code-mixed language.")
+
+	if(not args.tagDoc or args.dictDoc):
+		print("Must supply tagged corpus of code-mixed words or dictionary of words in given language.")
+
+	spellchecker = Spellchecker(args.langTag, args.repklEng, args.A, args.outputType, args.tagDoc, args.dictDoc)
+
 	with open(args.file) as f:
 		lines = f.readlines()
 		for line in lines:
